22907bb68c5d5adcb8eebee381f4108818df2d12
PrashantJsReact <prashant.ginims@gmail.com>
2023-07-03 19:12:38 +0530

    correct global config file username and email

--- js/app.js
+++ js/app.js
@@ -1,2 +1,4 @@
 console.log('He Javascript I am here to help you');
 console.log('Please help me too!');
+console.log("Github Desktop changed my global config username and email to the remote that's not fair things");
+console.log("while installing github it prompted for writing name email but i kept default so this problem happened and it changed my previous global config file")
